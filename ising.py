0fd7b31af5d70f47a08e8f7ce8d29637f3ee231e
miguel.aqs <mrmirosilva@gmail.comm>
2026-07-01 14:56:39 +0800

    Added a toggleable graph function to show net magnetization.

--- ising.py
+++ ising.py
@@ -28,28 +28,60 @@ def metropolis(spins, N, T, J=1.0):
     if dE <= 0 or random.random() < np.exp(-dE / T):
         spins[i, j] *= -1
 
+def get_net_magnetization(spins):
+    return np.mean(spins)
 
 
-#config stuff
-grid_size = 150
-T = 2.269
+"""---CONFIG STUFF---"""
+grid_size = 100
+T = 4
 steps_per_frame = 50000
+graph_length = 100
+show_graph = True
+
+"""------------------"""
+
+
+#note that -1 is black and 1 is white
 
 grid1 = init_lattice(grid_size)
 
-fig, ax = plt.subplots()
+if show_graph == True:
+    mag_history = []
 
-#-1 is black and 1 is white
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
 
-im = ax.imshow(grid1, cmap='gray', vmin=-1, vmax=1)
+    line, = ax2.plot([], [], color='black')
+
+
+    ax2.set_xlim(0, graph_length)
+    ax2.get_xaxis().set_visible(False)
+    ax2.set_ylim(-1.1, 1.1)
+else:
+    fig, (ax1) = plt.subplots(figsize=(6, 6))
+
+im = ax1.imshow(grid1, cmap='gray', vmin=-1, vmax=1)
+#tx = ax2.set_title(get_net_magnetization(grid1))
 
 def update(frame):
     for _ in range(steps_per_frame):
         metropolis(grid1, grid_size, T)
+    
     im.set_data(grid1)
-    return [im]
 
-# Create the live animation window
+    if show_graph == True:
+        net_mag = get_net_magnetization(grid1)
+        mag_history.append(net_mag)
+    
+        line.set_data(range(len(mag_history)), mag_history)
+        if len(mag_history) > 100:
+            ax2.set_xlim(0, len(mag_history))
+        return [im, line]
+    else:
+        return [im]
+    
+    
+
 ani = animation.FuncAnimation(fig, update, interval=10, blit=True, cache_frame_data=False)
 
 plt.show()
